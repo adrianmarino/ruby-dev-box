767ff0e340bb9651cda7fc7ab3918c3797fa2a7b
Adrian Marino <adrianmarino@gmail.com>
2015-01-25 15:28:13 -0300

    Improve output

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,40 @@
-# The output of all these installation steps is noisy. With this utility
-# the progress report is nice and concise.
+###############################################################################
+# Functions
+###############################################################################
 function install {
-    echo installing $1
+    echo "Installing $1"
     shift
     apt-get -y install "$@" >/dev/null 2>&1
 }
-
-echo updating package information
+#
+#
+#
+###############################################################################
+# Main
+###############################################################################
+echo "APT update"
 apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
 
-install 'development tools' build-essential
+install 'Development tools' build-essential
 
-install Ruby ruby2.2 ruby2.2-dev
-update-alternatives --set ruby /usr/bin/ruby2.2 >/dev/null 2>&1
-update-alternatives --set gem /usr/bin/gem2.2 >/dev/null 2>&1
-
-echo installing Bundler
-gem install bundler -N >/dev/null 2>&1
-
-install Git git
-install SQLite sqlite3 libsqlite3-dev
+install "Git" git
+install "SQLite" sqlite3 libsqlite3-dev
 
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-install MySQL mysql-server mysql-client libmysqlclient-dev
+install "MySQL" mysql-server mysql-client libmysqlclient-dev
 
 install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
-install 'ExecJS runtime' nodejs
+install 'NodeJS runtime' nodejs
 
-# Needed for docs generation.
+echo "Setup System Language"
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
-# rvm, ruby and rails
+echo "RVM"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 
-echo 'all set, rock on!'
+install "Vim" vim
+
+echo 'Enjoy!'
